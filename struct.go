30460903db1ca61cafe66382fa05f5530686f5b3
hjonatha <jonathan.hasard@ynov.com>
2025-09-15 10:37:24 +0200

    [ADD] struct.go

--- struct.go
+++ struct.go
@@ -5,3 +5,13 @@ import "fmt"
 Type SystemCombat struct {
 	selection string
 }
+
+Type Character struct {
+	genre string
+	nom string
+	classe string
+	niveau string
+	points de vie maximum string
+	points de vie actuels string
+	inventaire string
+}
