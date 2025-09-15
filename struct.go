3a3b095c88d67d8a532fee55fad5b9e1d92e28c5
Sazk47 <jonathan212006hasard@gmail.com>
2025-09-15 16:11:47 +0200

    [DELETE] putting my struct in another file in User branch

--- struct.go
+++ struct.go
@@ -6,13 +6,3 @@ type Monster struct {
 	current_hp   int
 	attack_point int
 }
-
-type Character struct {
-	genre      string
-	name       string
-	classe     string
-	level      int
-	hp_max     int
-	current_hp int
-	inventory  map[string]int
-}
