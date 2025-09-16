98ed4e7f8b87ef6ce8180c524c3cbd45af907964
Kinsley SUEDILE COLLET <kinsley.suedile-collet@epitech.eu>
2025-09-16 16:15:00 +0200

    [ADD] I add submenu in main.go but it is just an simple test.

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	menu := NewMenu("Game")
 	menu.AddItem("Start", 1)
+	menu2 := NewMenu("Party")
+	menu2.AddItem("New game", 1)
 	menu.AddItem("Option", 2)
 	menu.AddItem("Exit", 3)
 
@@ -18,5 +20,16 @@ func main() {
 		fmt.Printf("Selected option: %s\n", result)
 	} else {
 		fmt.Printf("Selected option of unexpected type: %T with value: %v\n", result, result)
+	}
+	result1, err1 := menu2.Display()
+	if err1 != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+	if _, ok := result1.(int); ok {
+		fmt.Printf("Selected option: %d\n", result1)
+	} else if _, ok := result1.(string); ok {
+		fmt.Printf("Selected option: %s\n", result1)
+	} else {
+		fmt.Printf("Selected option of unexpected type: %T with value: %v\n", result1, result1)
 	}
 }
