5b7caa5ef74271ef4a9eecd806019e7fb1eba233
Kinsley SUEDILE COLLET <kinsley.suedile-collet@epitech.eu>
2025-09-16 15:15:08 +0200

    [CHANGE] I changed menu for just get three simple options : start, option, exit.

--- main.go
+++ main.go
@@ -1,27 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-
-	menu := NewMenu("Chose a colour")
-
-	menu.AddItem("Langues", 1)
-	menu.AddItem("Commandes", 2)
-	menu.AddItem("Musiques", 3)
-	menu.AddItem("Star select niveau", 4)
-	menu.AddItem("Jouer", 5)
-	menu.AddItem("Retour", 6)
-	menu.AddItem("Quiter", 7)
-	menu.AddItem("Yellow", 8)
+	menu := NewMenu("Game")
+	menu.AddItem("Start", 1)
+	menu.AddItem("Option", 2)
+	menu.AddItem("Exit", 3)
 
 	result, err := menu.Display()
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
-
 	if _, ok := result.(int); ok {
 		fmt.Printf("Selected option: %d\n", result)
 	} else if _, ok := result.(string); ok {
