7def36972ed1f5bb1837bceadb76e8ca49ea8370
Kinsley SUEDILE COLLET <kinsley.suedile-collet@epitech.eu>
2025-09-18 12:03:21 +0200

    [FIX && CHANGE] I fixed the menufor submenu and change the language of menu for replace by french language.

--- init2_menu.go
+++ init2_menu.go
@@ -1,70 +1,88 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Options struct {
+	arg string
+	val int
+}
 
 func MainMenu() int {
-	menu := NewMenu(" ----- Game -----")
-	menu.AddItem("1.Start", 1)
-	menu.AddItem("2.Option", 2)
-	menu.AddItem("3.Exit", 3)
-
+	options := []Options{
+		{"Jouer", 1},
+		{"Options", 2},
+		{"Quitter", 3},
+	}
+	menu := NewMenu(" ----- StarForge -----")
+	for _, opti := range options {
+		menu.AddItem(opti.arg, opti.val)
+	}
 	result, err := menu.Display()
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
-	if _, ok := result.(int); ok {
-		fmt.Printf("Selected option: %d\n", result)
-	} else if _, ok := result.(string); ok {
+	switch v := result.(type) {
+	case int:
+		if v == 1 {
+			SubStart()
+		}
+		fmt.Printf("Selected option: %d\n", v)
+	case string:
 		fmt.Printf("Selected option: %s\n", result)
-	} else {
+	default:
 		fmt.Printf("Selected option of unexpected type: %T with value: %v\n", result, result)
 	}
 	return 0
 }
 
 func SubStart() {
-	var start string
-
-	menu2 := NewMenu("Party")
-	if start == "1.Start" {
-		menu2.AddItem("New game", 1)
-		menu2.AddItem("Back", 2)
-		result1, err1 := menu2.Display()
-		//if menu2.AddItem("Back") == "Back" {
-		//	return MainMenu()
-		//}
-		if err1 != nil {
-			fmt.Printf("Error: %v\n", err1)
+	option := []Options{
+		{"Nouvelle partie", 1},
+		{"Retour", 2},
+	}
+	menu2 := NewMenu("Partie")
+	for _, opti := range option {
+		menu2.AddItem(opti.arg, opti.val)
+	}
+	result1, err1 := menu2.Display()
+	if err1 != nil {
+		fmt.Printf("Error: %v\n", err1)
+	}
+	switch v := result1.(type) {
+	case int:
+		fmt.Printf("Selectionner l'option: %d\n\n", v)
+		if v == 2 {
+			MainMenu()
 		}
-		if _, ok := result1.(int); ok {
-			fmt.Printf("Selected option: %d\n", result1)
-		} else if _, ok := result1.(string); ok {
-			fmt.Printf("Selected option: %s\n", result1)
-		} else {
-			fmt.Printf("Selected option of unexpected type: %T with value: %v\n", result1, result1)
-		}
-	} else {
-		//function for other verification options
+	case string:
+		fmt.Printf("Selected option: %s\n\n", v)
+	default:
+		fmt.Printf("Selected option of unexpected type: %T with value: %v\n", result1, result1)
 	}
 }
 
-func SubOption() {
-	var option string
+func Start_GMenu() {
 
-	menu2 := NewMenu("Options")
-	if option == "2.Option" {
-		menu2.AddItem("Music", 1)
-		menu2.AddItem("Back", 2)
-		result1, err1 := menu2.Display()
-		if err1 != nil {
-			fmt.Printf("Error: %v\n", err1)
-		}
-		if _, ok := result1.(int); ok {
-			fmt.Printf("Selected option: %d\n", result1)
-		} else if _, ok := result1.(string); ok {
-			fmt.Printf("Selected option: %s\n", result1)
-		} else {
-			fmt.Printf("Selected option of unexpected type: %T with value: %v\n", result1, result1)
-		}
-	}
 }
+
+//func SubOption() {
+//
+//	menu2 := NewMenu("Options")
+//	if option == "2.Option" {
+//		menu2.AddItem("Music", 1)
+//		menu2.AddItem("Back", 2)
+//		result1, err1 := menu2.Display()
+//		if err1 != nil {
+//			fmt.Printf("Error: %v\n", err1)
+//		}
+//		if _, ok := result1.(int); ok {
+//			fmt.Printf("Selected option: %d\n", result1)
+//		} else if _, ok := result1.(string); ok {
+//			fmt.Printf("Selected option: %s\n", result1)
+//		} else {
+//			fmt.Printf("Selected option of unexpected type: %T with value: %v\n", result1, result1)
+//		}
+//	}
+//}
